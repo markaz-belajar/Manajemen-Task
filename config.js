e674394dbcf3731f535a7758a890e9ac47525e9c
markaz-belajar <markaz.blajar@gmail.com>
2026-05-03 01:31:32 +0700

    config.js

--- config.js
+++ config.js
@@ -1,7 +1,1 @@
-// ════════════════════════════════════════════════════════════
-//  MARKAZ BELAJAR — KONFIGURASI
-//  Isi GOOGLE_CLIENT_ID dengan Client ID dari Google Cloud Console
-//  Lihat panduan di SETUP.md
-// ════════════════════════════════════════════════════════════
-
-const GOOGLE_CLIENT_ID = "1034591379549-j2igs6339ekhu2qqqaf6onfganbuvmm5.apps.googleusercontent.com ";
+const GOOGLE_CLIENT_ID = "1034591379549-j2igs6339ekhu2qqqaf6onfganbuvmm5.apps.googleusercontent.com";
